261cc116d75013a42828a2ce20c22111a54ccd65
mdkhalidhasanovik <mdkhalidhasanovik@gmail.com>
2026-05-02 02:08:56 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -206,4 +206,5 @@ def get_number(msg):
 
     bot.send_message(msg.chat.id, "❌ OTP not received")
 
-bot.infinity_polling()
+bot.remove_webhook()
+bot.infinity_polling(skip_pending=True, timeout=10, long_polling_timeout=5)
